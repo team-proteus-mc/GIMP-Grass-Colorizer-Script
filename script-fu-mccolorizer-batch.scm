b32c4db5987445ee6e3f926482f282557f732fc2
SeveringHams <49352616+SeveringHams@users.noreply.github.com>
2024-08-30 05:02:38 -0400

    Update script-fu-mccolorizer-batch.scm
    Add option to number 01-16

--- script-fu-mccolorizer-batch.scm
+++ script-fu-mccolorizer-batch.scm
@@ -1,14 +1,13 @@
-(define (script-fu-mccolorizer-batch img drawmain dirPath prefix)
+(define (script-fu-mccolorizer-batch img drawmain dirPath prefix numbered)
 	(gimp-image-undo-disable img)
 	(let* 
 		(
 			(i 0)
             (vS (vector "79c05a" "8ab689" "bfb755" "59c93c" "55c93f" "88bb66" "86b87f" "64c73f" "86b783" "83b593" "80b497" "91bd59" "90814d" "8eb971" "6a7039" "507a32"))
-            (pathandprefix "")
         )
         (if (char-alphabetic? (car (string->list dirPath)))
-        	(set! pathandprefix (string-append dirPath "\\" prefix)) 
-        	(set! pathandprefix (string-append dirPath "/" prefix))
+        	(set! dirPath (string-append dirPath "\\" prefix)) 
+        	(set! dirPath (string-append dirPath "/" prefix))
         )
 		(while (< i 16)
 			(let* 
@@ -18,12 +17,12 @@
 					(vB (string->number (list->string (list (string-ref (aref vS i) 4) (string-ref (aref vS i) 5))) 16)) ;
 					(imgDupe (car (gimp-image-duplicate img)))
 					(inColor1 (list vR vG vB))
-					(fullpath (string-append 
-						pathandprefix 
-						(aref vS i)
-						".png")
-					)
+					(fullpath "")
 				)
+				(if (= numbered TRUE) 
+					(set! fullpath (string-append dirPath (list->string (cdr (string->list (number->string (+ i 101) 10)))) "-" (aref vS i) ".png"))
+					(set! fullpath (string-append dirPath (aref vS i) ".png"))
+				)						
 				(mccolorizer-func imgDupe inColor1)
             	(file-png-save RUN-NONINTERACTIVE imgDupe (car (gimp-image-get-active-drawable imgDupe)) fullpath fullpath FALSE 0 FALSE FALSE FALSE FALSE FALSE)
             	(gimp-image-delete imgDupe)
@@ -73,5 +72,6 @@
 SF-IMAGE "Image" 0
 SF-DRAWABLE "Drawable" 0
 SF-DIRNAME "Output Directory" ""
-SF-STRING "File name prefix (optional)" "grass_block_side_")
+SF-STRING "File name prefix (optional)" "grass_block_side_"
+SF-TOGGLE "Number output textures 01-16" FALSE)
 (script-fu-menu-register "script-fu-mccolorizer-batch" "<Image>/Colors/MC-Colorizer-Batch")
